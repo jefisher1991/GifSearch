5ac31ea9ff4e2f74aabe838f12d19d449751ee53
Julie Fisher <Julie@Julies-MacBook-Pro.local>
2017-03-11 09:36:55 -0600

    final homework

--- monuments.js
+++ monuments.js
@@ -1,37 +1,35 @@
-// When the user clicks on a button, the page should grab 10 static, non-animated gif images from the GIPHY API and place them on the page.
-// When the user clicks one of the still GIPHY images, the gif should animate. If the user clicks the gif again, it should stop playing.
-// Under every gif, display its rating (PG, G, so on).
-// This data is provided by the GIPHY API.
-// Only once you get images displaying with button presses should you move on to the next step.
-// Add a form to your page takes the value from a user input box and adds it into your topics array. Then make a function call that takes each topic in the array remakes the buttons on the page. 
 
 // the variables/ topics
 var topics = ["Taj Mahal", "Great Wall of China", "Stonehenge", "Colosseum", "Statue of Liberty"];
 
 
-$("searchbutton").click(var search = $("#searchText"); 
-// console.log(search);
-
-
 // creates the buttons at the top
-for (i = 0; i < topics.length; i++)
-    $("#topicsHTMLId").append("<button>" + topics[i] + "</button> ");
+function buttons() {
+    var html = "";
+    for (i = 0; i < topics.length; i++) {
+        html += "<button class=myTopics>" + topics[i] + "</button>";
+    }
+    $("#topicsHTMLId").html(html);
+}
 
-
-function displayMonuments(urlAnimate, urlStill) {
+// function that pulls in the lists created below, three parameters- this will actualy dispaly the images to the screen.
+function displayMonuments(urlAnimate, urlStill, rating) {
     var html = "";
     for (var i = 0; i < urlAnimate.length; i++) {
+
         var tempHTML = "<img class=gif src=" + urlStill[i] + " ";
+
         tempHTML += "data-still=" + urlStill[i] + " ";
         tempHTML += "data-animate=" + urlAnimate[i] + " ";
         tempHTML += "data-state='still' ";
-        tempHTML += " >";
+        tempHTML += ">";
+        tempHTML += "<span class=ratings> Rating: " + rating[i] + "</span>";
         html += tempHTML;
     }
     $("#displayMonuments").html(html);
 }
 
-
+// this function creates the Ajax data pull, and when it is returned it creates the lists of urls, stills, and ratings. 
 function data(topic) {
     // Constructing a queryURL using the animal name
     var queryURL = "http://api.giphy.com/v1/gifs/search?q=" +
@@ -48,31 +46,47 @@ function data(topic) {
             console.log(response);
             var urlList = []
             var stillList = []
+            var ratings = []
+            console.log(response);
             for (var gif of response.data) {
                 var myUrl = "http://i.giphy.com/" + gif.id + ".gif";
                 urlList.push(myUrl);
                 stillList.push(gif.images.original_still.url)
+                ratings.push(gif.rating)
+                console.log(ratings);
+               
+
             }
-            displayMonuments(urlList, stillList);
+            // sends the results to the display monuments function.
+            displayMonuments(urlList, stillList, ratings);
         });
 }
 
 $(document).ready(function() {
+// makes the search button active and pushes the new button topic to the topics array
+    buttons();
+    $(".searchButton").click(function() {
+        var newItem = $(".searchText").val();
+        topics.push(newItem);
+        buttons();
+    });
 
-    $("button").on("click", function() {
+
+// this is going to make the animation work properly. 
+    $("body").on("click", ".myTopics", function() {
         var currentTopic = $(this).text();
         data(currentTopic);
     });
 
     $("body").on("click", ".gif", function() {
-      var state = $(this).attr("data-state");
-    if (state === "still") {
-      $(this).attr("src", $(this).attr("data-animate"));
-      $(this).attr("data-state", "animate");
-    } else {
-      $(this).attr("src", $(this).attr("data-still"));
-      $(this).attr("data-state", "still");
-    }
+        var state = $(this).attr("data-state");
+        if (state === "still") {
+            $(this).attr("src", $(this).attr("data-animate"));
+            $(this).attr("data-state", "animate");
+        } else {
+            $(this).attr("src", $(this).attr("data-still"));
+            $(this).attr("data-state", "still");
+        }
     });
 
 });
